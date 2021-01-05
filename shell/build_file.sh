297c5e8560076068611aec40bdf010b8e53751b2
Thiago Montovaneli Albertino <thiago.montovaneli@skipthedishes.ca>
2021-01-04 21:55:53 -0600

    merged files

--- shell/build_file.sh
+++ shell/build_file.sh
@@ -8,9 +8,21 @@ for FILE in $FILES
 do		
 	MONTH=`echo $FILE | cut -c27-28`
 	YEAR=`echo $FILE | cut -c30-33`	
-	NEW_FILE="${FILE}_new"
-	echo $NEW_FILE
+	NEW_FILE="${FILE}"
+	#echo $NEW_FILE
 	sed 1d $FILE > $FILE.$$.tmp
-	sed -e "s/^/${MONTH};/" $FILE.$$.tmp > ../merged/$NEW_FILE 
+	sed -e "s/^/${YEAR},${MONTH},/" $FILE.$$.tmp > ../merged/$NEW_FILE 
 	rm $FILE.$$.tmp
 done
+
+cd ../merged
+
+cat *.csv >> temp.csv
+
+cat ../original/header.txt | cat - temp.csv > temp.csv_new
+
+mv temp.csv_new canada_weather.csv_
+
+rm *.csv
+
+mv canada_weather.csv_ canada_weather.csv
